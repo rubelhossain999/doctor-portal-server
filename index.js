901bdd3fff508e93be77d9db4cb16198908baa21
Rubel Hossain99 <rubel820746@gmail.com>
2022-11-19 22:30:15 +0600

    This is the Server site to the first comment

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+// Mongodb Data Base
+//const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.nqqpx5x.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.nqqpx5x.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// Server Running 
+app.get('/', async(req, res) =>{
+    res.send("This is The Doctor Portal Server Site");
+});
+
+app.listen(port, () => {
+    console.log(`This is ${port}`);
+});
